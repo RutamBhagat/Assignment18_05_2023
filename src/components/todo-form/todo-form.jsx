30e2f54b90096eba6e44e1e043510d0c582c3d33
RutamBhagat <rutambhagat@gmail.com>
2023-05-18 19:16:00 +0530

    fix errors

---
+++ src/components/todo-form/todo-form.jsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import { TodosContext } from "../../todo-context";
+import "./todo-form.scss";
+
+export const TodoForm = () => {
+  const { todos, setTodos } = React.useContext(TodosContext);
+  const [task, setTask] = React.useState("");
+
+  const handleAddTodo = () => {
+    if (task.trim() !== "") {
+      const newTodo = {
+        id: todos.length,
+        label: task,
+        checked: false,
+      };
+      setTodos([...todos, newTodo]);
+      setTask("");
+    }
+  };
+
+  const handleKeyUp = (e) => {
+    if (e.key === "Enter") {
+      handleAddTodo();
+    }
+  };
+
+  return (
+    <div className="todo-form">
+      <input
+        placeholder="Enter new task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        onKeyUp={handleKeyUp}
+      />
+      <button type="button" onClick={handleAddTodo}>
+        Add task
+      </button>
+    </div>
+  );
+};
